02ab11c012f27f51919c0cf5bc1d167f6c1e6c18
asbeeq <assylbek.zakrikchinov@gmail.com>
2021-10-17 10:14:39 +0600

    feat: add get handler

--- server.go
+++ server.go
@@ -92,3 +92,21 @@ func handleDelete(w http.ResponseWriter, r *http.Request) (err error) {
 	w.WriteHeader(204)
 	return
 }
+
+func hadleGet(w http.ResponseWriter, r *http.Request) (err error) {
+	id, err := strconv.Atoi(path.Base(r.URL.Path))
+	if err != nil {
+		return
+	}
+	post, err := retrieve(id)
+	if err != nil {
+		return
+	}
+	output, err := json.MarshalIndent(&post, "", "\t\t")
+	if err != nil {
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(output)
+	return
+}
